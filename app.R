ca1339aba402edd192187ae4f79aac6a10594efc
Max O’Reilly <maxoreilly@I-Crushed-the-Details.local>
2025-08-26 17:42:55 -0600

    Aug 26

--- app.R
+++ app.R
@@ -8,17 +8,16 @@ library(leaflet)
 library(ggplot2)
 library(arcgis)
 library(dplyr)
-source("helpers.R")
-counties <- readRDS("data/counties.rds")
+library(shinyBS)
 
-## Loading in data from ArcGIS Online
-statesST <- st_read("https://services3.arcgis.com/iuNbZYJOrAYBrPyC/ArcGIS/rest/services/States_forR/FeatureServer/0/query?where=1=1&outFields=*&f=geojson")
-statesWikiTest <- read.csv("/Users/maxoreilly/Desktop/opioidDataDashboard/myApp/OODAttempt_oneFile/App-1/us_states_wikipedia_links.csv")
-statesMerged <- left_join(statesST, statesWikiTest, by = c("NAME" = "State"))
-labels = sprintf("<strong>%s</strong><br/><a href='%s' target='_blank'>Link to Wikipedia Page</a>",
-                 statesMerged$NAME, statesMerged$Wikipedia_Link) %>% lapply(htmltools::HTML)
 
-## Loading in Image
+
+Wonder_Full <- readRDS("Wonder_Full.rds")
+States <- readRDS("States_noAK_noHI.rds")
+
+## source("Data.R")
+
+## Loading in Images
 image_path_1 <- "/Users/maxoreilly/Desktop/opioidDataDashboard/myApp/OODAttempt_oneFile/App-1/Figures/Average\ Drive\ Time\ for\ Methadone\ Provider\ -\ 2020.png"
 image_path_2 <- "/Users/maxoreilly/Desktop/opioidDataDashboard/myApp/OODAttempt_oneFile/App-1/Figures/SAMSHAOTPs.png"
 
@@ -31,12 +30,36 @@ my_theme <- bs_theme(
   code_font = font_google("Mulish")
 )
 
+## INTERACTIVITY ##
+if (interactive()) {
+  shinyApp(
 
-## UI START ##
+##################
+##################
+#### UI START ####
+##################
+##################
 ui <- page_navbar(
   theme = my_theme,
   title = "Opioid Overdose & Treatment Access Dashboard - TUSM",
   inverse = TRUE,
+  #nav_panel(
+   # title = "I'm still trying",
+  #  navset_underline(
+  #    nav_panel(
+  #      title = "tyler",
+  #    tags$img(
+  #      src = "https://www.donttaptheglass.com/images/og-image.png",
+  #    )
+       # ),
+    #  nav_panel(
+   #     title = "button",
+  #      actionButton(
+ #         "trigger", "Big Red Button"
+#        )
+#      ),
+#    ),
+ # ),
   nav_panel(
     title = "Splash Page",
     p("Welcome to the dashboard"),
@@ -59,101 +82,74 @@ ui <- page_navbar(
           "This dashboard combines data from various publicly available sources to show areas that are disproportionately impacted by the opioid crisis or have low or inequitable access to harm reduction and treatment services. Using tabs at the top, you can explore trends in fatal and non-fatal overdose, access to services, etc."
         ),
         ## scrolling info about different aspects of OOD crisis. 
+      ),
+      card(
+          title = "button",
+          actionButton(
+            "trigger", "Big Red Button"
+          ),
+        
       )
     )
     )
-    ),
-    
+ ),
+
   nav_panel(
-    title = "Graphs", 
-    p("Graphs of OOD"),
-    navset_card_tab(
-      height = 450,
-      nav_panel(
-        "Graph",
-        card_title("graph showing MOUD access"),
-        card_image(
-          file = image_path_1,
-                   height = 400,
-                   width = 500)
-      ),
-      nav_panel(
-        "Map",
-        card_title("Map showing MOUD access"),
-        card_image(
-          file = image_path_2,
-          height = 400,
-          width = 500),
-      ),
-      #card_header(
-      #  "how do we make this look not ridiculous"
-      #),
-      
-    )
-    
-  ),
-  # nav_panel(
-   # title = "Census Data Tutorial",
-  #  card(
-      #layout_sidebar(
-        #sidebar = sidebar(
-        #  helpText(
-        #    "Create demographic maps with information from the 2010 US census."
-        #  ),
-          
-          # selectInput(
-          #  "var",
-          #  "Choose a variable to display",
-         #   choices = c("Percent White", "Percent Black", "Percent Hispanic", "Percent Asian"),
-        #    selected = "Percent White"
-          #),
-          
-          # sliderInput(
-            #"vals",
-            #"Range of Interest",
-           # min = 0,
-          #  max = 100,
-         #   value = c(0,100)
-        #  ),
-       # ),
-      #  card(
-     #     plotOutput("map")
-    #    )
-   #   )
-  #  )
- # ),
-  nav_panel(
-    title = "Maps", 
-    p("Maps of OOD"),
+    ## make this a sidebar layout
+    title = "Overdoses", 
+    p("Overdoses"),
     card(
-      card_header("Which State?"),
-      selectInput(
-        "state",
-        "Select Option",
-        choices=c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-        ),
-        selected = "Alabama",
+      layout_sidebar(
+      sidebar = sidebar(
+       card_header("Which Data?"),
+       selectInput(
+         "ODorCount",
+         "Select Overdose Count or Rate",
+         choices = c("Overdose Count", "Overdose Rate"),
+         selected = "Overdose Rate"
+       ),
+       selectInput(
+         "year",
+         "Select Option",
+         choices=c("2018", "2019", "2020",
+                   "2021", "2022"),
+         selected = "2022",
       )
+      ),
+      leafletOutput("leaflet_map")
+      ),
     ),
-    card(
-      leaflet(statesST) %>% addTiles()%>% 
-        addPolygons(color = "gray", 
-                    weight = 1,
-                    highlightOptions = highlightOptions(weight = 5, "black"),
-                    popup = labels)
-    )
-    
   ),
+
+ nav_panel(
+   title = "Treatment Services", 
+   p("Access to Treatment Services for Opioid Use Disorder"),
+   navset_card_tab(
+     height = 450,
+     nav_panel(
+       "Graphs",
+       card_title("graph showing MOUD access"),
+       ## can the image have a popup with more info?
+       card_image(
+         file = image_path_1,
+         height = 400,
+         width = 500)
+     ),
+     nav_panel(
+       "Maps",
+       card_title("Map showing MOUD access"),
+       ## can the map have a popup with more info?
+       card_image(
+         file = image_path_2,
+         height = 400,
+         width = 500),
+     )
+   )
+ ),
+ 
   nav_panel(
-    title = "Sources and Acknowledgements", 
+    title = "Sources", 
     p("Thanks Tom and rstudio documentation!"),
-    #card(
-        card_header("a reminder"),
-        # card_image(
-        #  file = "https://www.donttaptheglass.com/images/og-image.png",
-      # can't figure out how to put my own image here
-       #)
-      #),
   ),
   nav_spacer(),
   nav_menu(
@@ -162,12 +158,74 @@ ui <- page_navbar(
     nav_item(tags$a("Posit", href = "https://posit.co")),
     nav_item(tags$a("Shiny", href = "https://shiny.posit.co"))
   ),
-)
+),
 
 
-
+##################
+##################
 ## SERVER START ##
-server <- function(input, output) {
+##################
+##################
+server <- function(input, output, session) {
+  map_data <- reactive({
+    req(input$year, input$ODorCount)
+    if (input$ODorCount == "Overdose Rate") {
+      col = paste0("Crude_Rate_", input$year)
+    }
+    else {
+      col = paste0("Deaths_", input$year)
+    }
+    Wonder_Full %>% 
+      select(NAME, all_of(col))
+    }
+  )
+  
+  reactive_pal <- reactive({
+    vals <- map_data()[[2]]
+    print(vals)
+    colorBin(
+      "Oranges", 
+      domain = vals, 
+      bins = 5,
+      #pretty = TRUE,
+      na.color = "grey")
+  })
+  
+  popups_2 <- reactive({
+    vals <- map_data()[[2]]
+    name <- map_data()[[1]]
+    
+    labels <- paste0(
+      name, "<br/>", vals
+    )
+    lapply(labels, htmltools::HTML)
+  })
+  
+  
+  output$leaflet_map <- renderLeaflet({
+    pal <- reactive_pal()
+      leaflet(map_data()) %>%
+      setView(lng = -98.274923, lat =  38.904775, zoom = 4) %>% 
+      addPolygons(data = States,
+                  color = "black",
+                  weight = 2,
+                  opacity = 0.9) %>%
+      addPolygons(fillColor = ~pal(map_data()[[2]]), 
+                    weight = 1,
+                    color = "grey",
+                    highlightOptions = highlightOptions(weight = 5, "black"),
+                    popup = popups_2(),
+                    fillOpacity = 0.8) %>%
+      addLegend(
+        pal = reactive_pal(),
+        values = map_data()[[2]],
+        position = "bottomright"
+      )
+    
+  })
+  
+  
+  
   # pseudocode re: how to query by clicking // selecting a state
   # 2 options: 
   # 1: user selects state in the dropdown
@@ -177,31 +235,130 @@ server <- function(input, output) {
   # 2: the popup thing contains some sort of reference to another table that has links to 
   #.   the info they're looking for
   # labels = sprintf("<strong>Label</strong><br/>%s",
-    #.   statesST$NAME, "<br/>%s", tabelwithdata$LINK where states$name = table$name) %>% lapply(htmltools::HTML)
+    #.   States$NAME, "<br/>%s", tabelwithdata$LINK where states$name = table$name) %>% lapply(htmltools::HTML)
   
-
+  toggleModal(session, "BS", toggle = "toggle")
   
-  output$map <- renderPlot({
-    data <- switch(input$var,
-                   "Percent White" = counties$white,
-                   "Percent Black" = counties$black,
-                   "Percent Hispanic" = counties$hispanic,
-                   "Percent Asian" = counties$asian)
-    
-    color <- switch(input$var,
-                    "Percent White" = "darkgreen",
-                    "Percent Black" = "black",
-                    "Percent Hispanic" = "darkorange",
-                    "Percent Asian" = "darkviolet")
-    
-    legend <- switch(input$var,
-                     "Percent White" = "% White",
-                     "Percent Black" = "% Black",
-                     "Percent Hispanic" = "% Hispanic",
-                     "Percent Asian" = "% Asian")
-    
-    percent_map(data, color, legend, input$vals[1], input$vals[2])
+  observeEvent(input$trigger, {
+    showModal(modalDialog(
+      title = "Important",
+      size = "xl",
+      easyClose = TRUE,
+      card(
+            card_header("a reminder"),
+            card_image(
+              file = "https://www.donttaptheglass.com/images/og-image.png",
+              # can't figure out how to put my own image here
+           )
+        )
+    ))
   })
-}
+  
+  observeEvent(input$popup, {
+    showModal(modalDialog(
+      title = "Graph!",
+      "GRAPH GRAPH GRAPH"
+    ))
+  })
 
+}
+)
+}
 shinyApp(ui, server)
+
+
+
+#    card(
+#      height = 800,
+#     leaflet(Wonder_21_Raw) %>% setView(lng = -98.274923, lat =  38.904775, zoom = 4) 
+#    %>%
+#       addPolygons(fillColor = ~bin_pal_CR(Crude_Rate), 
+#                   weight = 1,
+#                   color = "grey",
+#                  highlightOptions = highlightOptions(weight = 5, "black"),
+#                  popup = labels,
+#                 fillOpacity = 0.8) 
+#  %>%
+#     addLegend(data = States, pal = bin_pal, title = "Legend", values = land, position = 'bottomright'
+#     )
+# )
+
+
+  
+  #nav_panel(
+  # title = "bitchass",
+  #p("fucking labels"),
+  #card(
+  #  height = 800,
+  #  leaflet(Wonder_21_Raw) %>% setView(lng = -98.274923, lat =  38.904775, zoom = 4)  %>%
+  #    addPolygons(data = States,
+  #                color = "black",
+  #               weight = 2,
+  #              opacity = 0.8) %>%
+  # addPolygons(fillColor = ~bin_pal_Deaths(Deaths), 
+  #            weight = 1,
+  #           color = "grey",
+  #          highlightOptions = highlightOptions(weight = 5, "black"),
+  #         popup = labels,
+  #        fillOpacity = 0.8) 
+  #)
+  #),
+  
+
+
+
+## LINKS TO DOCUMENTATION ## 
+# https://shiny.posit.co/r/reference/shiny/1.8.0/modaldialog.html
+
+# nav_panel(
+# title = "Census Data Tutorial",
+#  card(
+#layout_sidebar(
+#sidebar = sidebar(
+#  helpText(
+#    "Create demographic maps with information from the 2010 US census."
+#  ),
+
+# selectInput(
+#  "var",
+#  "Choose a variable to display",
+#   choices = c("Percent White", "Percent Black", "Percent Hispanic", "Percent Asian"),
+#    selected = "Percent White"
+#),
+
+# sliderInput(
+#"vals",
+#"Range of Interest",
+# min = 0,
+#  max = 100,
+#   value = c(0,100)
+#  ),
+# ),
+#  card(
+#     plotOutput("map")
+#    )
+#   )
+#  )
+# ),
+
+#coutput$map <- renderPlot({
+  #data <- switch(input$var,
+  #              "Percent White" = counties$white,
+  #               "Percent Black" = counties$black,
+   #              "Percent Hispanic" = counties$hispanic,
+  #               "Percent Asian" = counties$asian)
+ # 
+#  color <- switch(input$var,
+  #                "Percent White" = "darkgreen",
+ #                 "Percent Black" = "black",
+ #                 "Percent Hispanic" = "darkorange",
+#                "Percent Asian" = "darkviolet")
+ # 
+#  legend <- switch(input$var,
+  #                 "Percent White" = "% White",
+  #                 "Percent Black" = "% Black",
+  #                 "Percent Hispanic" = "% Hispanic",
+  #                 "Percent Asian" = "% Asian")
+  #
+ # percent_map(data, color, legend, input$vals[1], input$vals[2])
+#})
